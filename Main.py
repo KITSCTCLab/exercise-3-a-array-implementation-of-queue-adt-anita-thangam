c58b7545d84be08495a985f4611f744f58e8f423
anita-thangam <110010649+anita-thangam@users.noreply.github.com>
2022-08-14 11:46:29 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,75 +1,80 @@
 class Solution:
- def     init    (self, size):
-  self.size = size self.lst = [None]*size
-  self.queue = [None]*size 
-  self.top = -1
-  self.rear = -1
-  self.front = -1
+    def __init__(self, size):
+        self.stack = []
+        self.queue = []
+        self.size = size
+        self.top = -1
+        self.rear = -1
+        self.front = -1
 
- def is_stack_empty(self):
-  if self.top == -1:
-    return 1 
-   else :
-    return 0
- def is_queue_empty(self):
-  if self.front==-1 or self.front>self.rear: 
-   return 1
-  else:
-   return 0
-def is_stack_full(self):
-  if self.top == (self.size - 1): 
-   return 1
-  else :
-   return 0
-def is_queue_full(self):
-  if self.rear==(self.size-1):
-   return 1
-  else:
-   return 0
-def push_character(self, character): 
-  if not self.is_stack_full():
-   self.top+=1
- 
+    def is_stack_empty(self):
+        if self.top == -1:
+            return True
+        else:
+            return False
 
-   self.lst[self.top]=character
-def enqueue_character(self, character): 
- if not self.is_queue_full():
- if self.front==-1: 
-    self.front=0
-    self.rear+=1 
-    self.queue[self.rear]=character
-def pop_character(self):
-  if not self.is_stack_empty(): 
-    temporary=self.lst[self.top]
-    del self.lst[self.top]
-    self.top-=1
-    return temporary
-def dequeue_character(self):
-  if not self.is_queue_empty():
-    req=self.queue[0]
-    del self.queue[0] 
-    self.front+=1
-    return req
- text = input()
+    def is_queue_empty(self):
+        if self.rear == -1 and self.front == -1:
+            return True
+        else:
+            return False
+
+    def is_stack_full(self):
+        if self.size == len(self.stack):
+            return True
+        else:
+            return False
+
+    def is_queue_full(self):
+        if self.size == len(self.queue):
+            return True
+        else:
+            return False
+
+    def push_character(self, character):
+        if not self.is_stack_full():
+            self.stack.append(character)
+            self.top += 1
+                    
+    def enqueue_character(self, character):
+        if not self.is_queue_full():
+            self.queue.append(character)
+            self.rear += 1
+            if self.front == -1:
+                self.front += 1
+
+    def pop_character(self):
+        if not self.is_stack_empty():
+            data = self.stack.pop()
+            self.top -= 1
+            return data
+
+    def dequeue_character(self):
+        if not self.is_queue_empty():
+            data = self.queue[self.front]
+            self.front += 1
+            return data
+        
 
 
+text = input()
+
+length_of_text = len(text)
 
 solution = Solution(length_of_text)
-solution.push_character(text[index]) solution.enqueue_character(text[index])
 
-is_palindrome = True a=""
- 
+for index in range(length_of_text):
+    # Write code here
+    solution.push_character(text[index])
+    solution.enqueue_character(text[index])
 
-b=""
-for i in range(solution.front,solution.rear+1): a+=solution.pop_character() b+=solution.dequeue_character()
-if a!=b:
-is_palindrome = False else:
 is_palindrome = True
+for index in range(length_of_text):
+    if  solution.pop_character() != solution.dequeue_character():
+        is_palindrome = False
+        break
 
-print("The word, " + text + ", is a palindrome.") else:
-print("The word, " + text + ", is not a palindrome.")
-
-
-
-
-
+if is_palindrome:
+    print("The word, " + text + ", is a palindrome.");
+else:
+    print("The word, " + text + ", is not a palindrome.")
